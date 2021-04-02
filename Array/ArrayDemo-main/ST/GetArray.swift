d3d9f599c0f63bda2db70681e34e198e7b09f58f
Askar Ulubayev <askar.ulubayev@gmail.com>
2021-04-02 09:13:13 +0600

    first commit

---
+++ Array/ArrayDemo-main/ST/GetArray.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+extension NSArray {
+    static func trueArray() -> NSArray {
+        var arr: NSArray?
+        let url = URL(fileURLWithPath: FileManager.default.currentDirectoryPath).appendingPathComponent("array.plist")
+        autoreleasepool {
+            arr = NSArray(contentsOf: url) ?? []
+        }
+
+        return arr!
+    }
+}
+
+extension Array where Element == Data {
+    static func trueArray() -> Self {
+        var arr: Self?
+        autoreleasepool {
+            arr = Array(ContiguousArray(NSArray.trueArray() as! [Data]))
+        }
+        return arr!
+    }
+}
